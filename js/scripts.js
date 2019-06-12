896ea8778b80ac77044049a4b9e8521e15aeaf4b
Holden Clark and Na Hyung Choi <holden@>
2019-06-12 15:22:04 -0700

    convert numbers that are multiples of base Roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,36 @@
 var userNumber;
-var romanNumeral;
+var romanNumeral = "";
 
-var symbols = ["I", "V", "X", "L", "C", "D", "M"];
-var symbolValues = [1, 5, 10, 50, 100, 500, 1000];
+var symbols = ["M", "D", "C", "L", "X", "V", "I"];
+var symbolValues = [1000, 500, 100, 50, 10, 5, 1];
 
-var converter = function(number) {
-  if (number > 3999) {
-    return "Please enter a number less than 4,000"
-  } else {
-    symbolValues.forEach(function(value) {
-      if (number === value) {
-        romanNumeral = symbols[symbolValues.indexOf(value)];
-      }
-    })
+var numeralDuplicator = function(multiplier, index){
+  for (var i = 0; i < multiplier; i++){
+    romanNumeral += symbols[symbolValues.indexOf(index)];
   }
 }
 
+var converter = function(input) {
+  // debugger;
+  if (input > 3999) {
+    return "Please enter a number less than 4,000";
+  } else {
+    for (var i = 0; i < symbolValues.length; i++){
+      if (input === symbolValues[i]) {            // input: 10, output: X
+        romanNumeral = symbols[i];
+        break;
+      } else if (input % symbolValues[i] === 0){
+        numeralDuplicator((input/symbolValues[i]), symbolValues[i]);
+        break;
+      }
+    }
+  }
+}
 
 $(function(){
   $("#user-input").submit(function(event){
     event.preventDefault();
+    romanNumeral = "";
 
     userNumber = parseInt($("#userInput").val());
     converter(userNumber);
