6536f7ebea1c7c5904d96c38c8dd6ae3f37207a0
Holden Clark and Na Hyung Choi <holden@>
2019-06-12 16:52:10 -0700

    convert numbers that only require addition

--- js/scripts.js
+++ js/scripts.js
@@ -10,18 +10,33 @@ var numeralDuplicator = function(multiplier, index){
   }
 }
 
+var numeralSubtractor = function(index){
+  var tempNumeral = symbols[symbolValues.indexOf(index)];
+  romanNumeral = tempNumeral + romanNumeral;
+}
+
 var converter = function(input) {
-  // debugger;
+  debugger;
   if (input > 3999) {
     return "Please enter a number less than 4,000";
   } else {
     for (var i = 0; i < symbolValues.length; i++){
       if (input === symbolValues[i]) {            // input: 10, output: X
-        romanNumeral = symbols[i];
+        romanNumeral += symbols[i];
         break;
-      } else if (input % symbolValues[i] === 0){
+      } else if (input % symbolValues[i] === 0){      // input: 20, output: XX
         numeralDuplicator((input/symbolValues[i]), symbolValues[i]);
         break;
+      } else if (input > symbolValues[i]) {       // input: 21, output: XXI
+        numeralDuplicator(Math.floor(input/symbolValues[i]), symbolValues[i]);
+        input = input-(Math.floor(input/symbolValues[i])*symbolValues[i]);
+      // } else if (symbolValues[i] === input+1 || symbolValues[i] === input - 4) {     // detects 4's and 9's (e.g., 5 === 4+1)
+      //   romanNumeral = romanNumeral + symbols[i+1] + symbols[i];
+      //   break;
+        // numeralDuplicator(1, symbolValues[i]);
+        // input = input - symbolValues[i];
+      } else if (input < 0){
+        numeralSubtractor(symbolValues[i]);
       }
     }
   }
